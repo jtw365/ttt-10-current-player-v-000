52581a312de5eb3d0ac154a4c6d4ce4200b21b96
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-05 10:38:20 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,8 @@
 def turn_count(board)
-  turns = ["turn 1", "turn 2", "turn 3", "turn 4", "turn 5","turn 6", "turn 7", "turn 8", "turn 9"]
-  until turns == 9
-    turns += 1
+  turn_count = 0
+
+  board.each do |space|
+    if space == "O" or space == "X"
+      turn_count += 1
   end
 end
